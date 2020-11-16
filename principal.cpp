e44d193f934e417555c9dc09a82618dba72fe4c3
mamaniburgosluisandres@gmail.com <mamaniburgosluisandres@gmail.com>
2020-11-15 16:30:25 -0800

    se modifico

---
+++ principal.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout << " nombre: Mamani Burgos Luis Andres" << endl;
+	cout << " ci: 10554719" << endl;
+	cout << "cu: 35-3715" << endl;
+	//cambio//
+}
